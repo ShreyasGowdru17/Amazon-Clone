8d4fc0ecf93b03b71c580f17caf735c91a802652
ShreyasGowda <shreyaskgowda17@gmail.com>
2025-03-10 23:36:29 +0530

    save cart in local storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,33 +1,34 @@
 import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import { formatCurrency} from './utils/money.js';
+import {formatCurrency} from './utils/money.js';
 
 let cartSummaryHTML='';
 
 cart.forEach((cartItem)=>{
   const productId=cartItem.productId;
 
-  let matchingItem;
+  let matchingProduct;
   products.forEach((product)=>{
       if(product.id===productId){
-        matchingItem=product;
+        matchingProduct=product;
       }
-  })
-  cartSummaryHTML+=`<div class="cart-item-container js-cart-item-container-${productId}">
+  });
+  console.log(matchingProduct.image);
+  cartSummaryHTML+=`<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
 
       <div class="cart-item-details-grid">
         <img class="product-image"
-          src="${matchingItem.image}">
+          src="${matchingProduct.image}">
 
         <div class="cart-item-details">
           <div class="product-name">
-            ${matchingItem.name}
+            ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $${formatCurrency(matchingItem.priceCents)}
+            $${formatCurrency(matchingProduct.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
@@ -36,7 +37,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingItem.id}>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
               Delete
             </span>
           </div>

8d207e3227983dfef6bfbcac53915937856f21b7
ShreyasGowda <shreyaskgowda17@gmail.com>
2025-03-16 14:37:15 +0530

    make localstroage key and load from storage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,15 +1,15 @@
 class Cart{
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey){
-    this.localStorageKey=localStorageKey;
+    this.#localStorageKey=localStorageKey;
     this.loadFromStorage();
     this.updateCheckOut();
   }
 
   loadFromStorage(){
-    this.cartItems=JSON.parse(localStorage.getItem(this.localStorageKey));
+    this.cartItems=JSON.parse(localStorage.getItem(this.#localStorageKey));
     if(this.cartItems===null || !this.cartItems ){
       this.cartItems=[{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -25,7 +25,7 @@ class Cart{
   }
 
   saveToStorage(){
-    localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey,JSON.stringify(this.cartItems));
   }
 
   updateCheckOut(){
@@ -82,7 +82,7 @@ class Cart{
         newCart.push(cartItem);
       }
     });
-    this.cartItem=newCart;
+    this.cartItems=newCart;
     this.saveToStorage();
     this.updateCheckOut();  
   }
@@ -106,6 +106,7 @@ class Cart{
 const cart= new Cart('cart-oop');
 const businessCart=new Cart('business-cart');
 
+cart.#localStorageKey='aa';
 
 console.log(cart);
 console.log(businessCart);
